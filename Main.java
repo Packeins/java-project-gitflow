20c4b9d8615d8c239c4755621d4221387d4ab7a2
Esteban Carvajal <packeins@gmail.com>
2025-12-18 15:23:08 -0500

    release: agregar Ejercicio 2: Preparar una Nueva Versión - v1.1.0

--- Main.java
+++ Main.java
@@ -1,10 +1,13 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
         Despedida despedida = new Despedida();
         System.out.println(despedida.generarDespedida("Mundo"));
-        System.out.println("Versión 1.0.1 lista para producción.");
+        System.out.println("Versión 1.1.0 lista para producción.");
     }
     
     
